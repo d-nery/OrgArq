e93d40284a4d9771c7b40cc41ba124e45dcfb9d8
Daniel Nery <danielnso97@gmail.com>
2018-07-05 01:28:25 -0300

    UC port definition

--- PicoMIPS/UC.vhd
+++ PicoMIPS/UC.vhd
@@ -16,24 +16,31 @@ use work.types.all;
 
 entity UC is
     port (
-        clk: in std_logic;
-        rst: in std_logic;
+        clk:   in std_logic;
+        rst:   in std_logic;
+        pc_wr: out std_logic;
+
+        icache_en:   out  std_logic;
+        icache_done: in std_logic;
 
         opcode: in std_logic_vector(5 downto 0);
         funct:  in std_logic_vector(5 downto 0);
 
         reg_write:   out std_logic;
 
-        dmem_enable: out std_logic;
-        dmem_rw:     out std_logic;
+        mux_rbwr:    out std_logic;
+        mux_alusrc:  out std_logic;
+        mux_wb:      out std_logic;
+        mux_mem_src: out std_logic;
+        mux_pcsrc1:  out std_logic;
+        mux_pcsrc2:  out std_logic;
 
-        mux_memtoreg: out std_logic;
-        mux_regdest:  out std_logic;
-        mux_ulasrc:   out std_logic;
-        mux_newPC:    out std_logic_vector(1 downto 0);
+        ula_zero:    in std_logic;
+        ula_control: out  nibble_t;
 
-        ula_zero: in  std_logic;
-        ula_op:   out nibble_t
+        dcache_ready: in std_logic;
+        dcache_wr:    out  std_logic;
+        dcache_en:    out  std_logic
     );
 end entity UC;
 
@@ -67,31 +74,31 @@ begin
             when OP_SW   => prox_estado <= SW;
             when others  => prox_estado <= INICIO;
         end case;
-    end process state; 
+    end process state;
 
-    with estado_atual select
-        mux_regdest <= '1' when R,
-                        '0' when others;
+    -- with estado_atual select
+    --     mux_regdest <= '1' when R,
+    --                     '0' when others;
 
-    with estado_atual select
-        mux_memtoreg <= '1' when LW,
-                        '0' when others;
+    -- with estado_atual select
+    --     mux_memtoreg <= '1' when LW,
+    --                     '0' when others;
 
-    with estado_atual select
-        mux_ulasrc <= '1' when ADDI | LW | SW,
-                      '0' when others;
+    -- with estado_atual select
+    --     mux_ulasrc <= '1' when ADDI | LW | SW,
+    --                   '0' when others;
 
-    with estado_atual select
-        reg_write <= '1' when R | ADDI| LW,
-                     '0' when others;
+    -- with estado_atual select
+    --     reg_write <= '1' when R | ADDI| LW,
+    --                  '0' when others;
 
-    with estado_atual select
-        dmem_enable <= '0' when INICIO,
-                      '1' when others;
+    -- with estado_atual select
+    --     dmem_enable <= '0' when INICIO,
+    --                   '1' when others;
 
-    with estado_atual select
-        dmem_rw <= '1' when SW,
-                   '0' when others;
+    -- with estado_atual select
+    --     dmem_rw <= '1' when SW,
+    --                '0' when others;
 
     -- with estado_atual select
     --     mux_newPC <= "00" when INICIO,
@@ -101,5 +108,5 @@ begin
     --     ula_op <= "0000" when INICIO,
     --               "0001" when others;
 
-    dmem_rw <= not clk and we;
+    -- dmem_rw <= not clk and we;
 end architecture UC_arch;
