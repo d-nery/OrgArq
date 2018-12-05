7e044e66c98984ba3a137b259b77da32a7768641
Beatricce <beatriz.de.oli.s@gmail.com>
2018-12-05 16:43:44 -0200

    Correção BTB

--- PicoMIPS/Pipeline/CP.vhd
+++ PicoMIPS/Pipeline/CP.vhd
@@ -21,6 +21,7 @@ entity cp is
     port (
 		clk : in std_logic;
         in_address: in  std_logic_vector(31 downto 0);
+		target_address: inout  std_logic_vector(31 downto 0);
 		hit: in std_logic;
 		taken_prev: in std_logic;
 		taken_dec: in std_logic
@@ -29,34 +30,50 @@ end entity cp;
 
 architecture cp_arch of cp is
 	signal hiti, taken_previ, taken_deci, erase, enable, penalty: std_logic;
-	signal in_address, out_address: std_logic_vector(31 downto 0);
+	signal ini_address, targeti_address: std_logic_vector(31 downto 0);
 	
 	component btb 
 	port (
 		clk : in std_logic;
         in_address: in  std_logic_vector(31 downto 0);
-        out_address: out  std_logic_vector(31 downto 0);
+        target_address: inout  std_logic_vector(31 downto 0);
 		hit: out std_logic;
 		erase: in std_logic;
 		enable: in std_logic
 	);
 	end component;
 	
-	begin	
+	signal erasei: std_logic;
+	signal enablei: std_logic;
+	
+	begin
+		
+		btb1: btb port map (clk => clk, in_address => ini_address, target_address => targeti_address, hit=> hiti, erase => erasei, enable=>enablei );
+		
+		process(clk)
+		begin
 		hiti <= hit;
 		taken_previ <= taken_prev;
 		taken_deci <= taken_dec;
 		
 		if hiti = '1' then
 			penalty <= taken_deci nand taken_previ;
-			if penalty = '0' then
-				btb1: btb port map(clk => clk, in_address => in_address, out_address => out_address, hit=> hiti, erase => '0', enable=>'0' );
+			if penalty = '1' then
+				erasei <= '1';
+				enablei <= '0';
 			else 
-				btb2: btb port map(clk => clk, in_address => in_address, out_address => out_address, hit=> hiti, erase => '1', enable=>'0' );
+				erasei <= '0';
+				enablei <= '0';
+			end if; 
 		else 
 			penalty <= taken_deci nor taken_previ;
 			if penalty = '0' then
-				btb3: btb port map(clk => clk, in_address => in_address, out_address => out_address, hit=> hiti, erase => '0', enable=>'0' );
+				erasei <= '0';
+				enablei <= '0';
 			else 
-                btb4: btb port map(clk => clk, in_address => in_address, out_address => out_address, hit=> hiti, erase => '0', enable=>'1' );
+				enablei <= '1';
+				erasei <= '0';
+			end if;
+		end if;
+		end process;		
 end architecture cp_arch;
